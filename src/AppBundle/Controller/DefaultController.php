72cecf7e79fc300579969f025aa386a7051b2b69
Giustino Borzacchiello <giustinob@gmail.com>
2016-02-07 20:32:34 +0100

    feat: create /quote endpoint

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,17 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/quote", name="random_quote")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function quoteAction(Request $request)
+    {
+        return new Response('<html><body>This is a random quote</body></html>');
     }
 }
